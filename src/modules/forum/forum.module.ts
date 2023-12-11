97edfd50754e257a7b9d2d270bece7f0c952dfd8
AndriiNemirov <nemirov.andrei@gmail.com>
2023-12-11 11:05:15 +0200

    forum module

--- src/modules/forum/forum.module.ts
+++ src/modules/forum/forum.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from './categories/categories.module';
+import { CategoriesService } from './categories/categories.service';
+import { TopicsModule } from './topics/topics.module';
 
-@Module({})
+@Module({
+  imports: [CategoriesModule, TopicsModule],
+  providers: [CategoriesService],
+})
 export class ForumModule {}
